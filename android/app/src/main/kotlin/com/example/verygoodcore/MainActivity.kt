b2ccc02976a7d344489f3c170bb151a3c4b90281
prabesh12 <prabesholi8848@gmail.com>
2023-03-08 21:49:53 -0800

    project initialized

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.flavor
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_staging.dart
@@ -0,0 +1,6 @@
+import 'package:flavor/app/app.dart';
+import 'package:flavor/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
